fc6e575c563ad18b3cd4f2cb2c76ad4cc783f834
vladimir-12343 <96837504+vladimir-12343@users.noreply.github.com>
2023-12-07 14:33:51 +0300

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,21 @@
+import time
+
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Функция {func.__name__} выполнилась за {execution_time:.4f} секунд")
+        return result
+    return wrapper
+
+@timing_decorator
+def fibonacci():
+    fib1 = fib2 = 1
+    for i in range(2, 200):
+        fib1, fib2 = fib2, fib1 + fib2
+        print(fib2, end='\n')
+
+if __name__ == '__main__':
+    fibonacci()
